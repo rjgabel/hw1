c6c9a3224a9ea275e7f747391e4aac5c1f90eea5
Ryan Gabel <rjgabel@usc.edu>
2022-08-30 11:39:40 -0700

    a

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -24,7 +24,113 @@ size_t ULListStr::size() const
   return size_;
 }
 
-// WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val) {
+    if (empty()) {
+        // list is empty
+        Item* node = new Item();
+        head_ = node;
+        tail_ = node;
+    } else if (tail_->last == ARRSIZE) {
+        // No more space in the back of this node, make a new one
+        Item* node = new Item();
+        node->prev = tail_;
+        tail_->next = node;
+        tail_ = node;
+    }
+    tail_->val[tail_->last] = val;
+    tail_->last++;
+    size_++;
+}
+
+void ULListStr::pop_back() {
+    if (empty()) {
+        // list is empty, nothing to pop!
+        return;
+    }
+    tail_->last--;
+    size_--;
+    if (empty()) {
+        delete tail_;
+        head_ = nullptr;
+        tail_ = nullptr;
+    } else if (tail_->last == 0) {
+        Item* new_tail = tail_->prev;
+        delete tail_;
+        tail_ = new_tail;
+        tail_->next = nullptr;
+    }
+}
+
+void ULListStr::push_front(const std::string& val) {
+    if (empty()) {
+        // list is empty
+        Item* node = new Item();
+        head_ = node;
+        tail_ = node;
+        head_->first = ARRSIZE;
+        head_->last = ARRSIZE;
+    } else if (head_->first == 0) {
+        // No more space in the front of this node, make a new one
+        Item* node = new Item();
+        node->next = head_;
+        head_->prev = node;
+        head_ = node;
+        head_->first = ARRSIZE;
+        head_->last = ARRSIZE;
+    }
+    head_->val[head_->first - 1] = val;
+    head_->first--;
+    size_++;
+}
+
+void ULListStr::pop_front() {
+    if (empty()) {
+        // list is empty, nothing to pop!
+        return;
+    }
+    head_->first++;
+    size_--;
+    if (empty()) {
+        delete head_;
+        head_ = nullptr;
+        tail_ = nullptr;
+    } else if (head_->first == ARRSIZE) {
+        Item* new_head = head_->next;
+        delete head_;
+        head_ = new_head;
+        head_->prev = nullptr;
+    }
+}
+
+std::string const& ULListStr::back() const {
+    if (empty()) {
+        throw std::invalid_argument("Bad location");
+    }
+    return tail_->val[tail_->last - 1];
+}
+
+std::string const& ULListStr::front() const {
+    if (empty()) {
+        throw std::invalid_argument("Bad location");
+    }
+    return head_->val[head_->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+    if (loc >= size()) {
+        return nullptr;
+    }
+    Item* node = head_;
+    size_t index = head_->first;
+    for (int i = 0; (size_t)i < loc; i++) {
+        index++;
+        if (index == ARRSIZE) {
+            node = node->next;
+            index = 0;
+        }
+    }
+    return &node->val[index];
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
--- ulliststr_test.cpp
+++ ulliststr_test.cpp
@@ -1,11 +1,61 @@
 /* Write your test code for the ULListStr in this file */
 
+#include "ulliststr.h"
 #include <iostream>
 using namespace std;
 
+void test(bool condition) {
+    if (!condition) {
+        std::cout << "Test failed\n";
+        exit(1);
+    }
+}
 
-int main(int argc, char* argv[])
-{
-
-  return 0;
+int main(int argc, char* argv[]) {
+    const std::string astley[28] = {
+        "We're",    "no",  "strangers", "to",           "love", "You",
+        "know",     "the", "rules",     "and",          "so",   "do",
+        "I",        "A",   "full",      "commitment's", "what", "I'm",
+        "thinking", "of",  "You",       "wouldn't",     "get",  "this",
+        "from",     "any", "other",     "guy"};
+    ULListStr* list = new ULListStr();
+    test(list->size() == 0);
+    test(list->empty());
+    for (int i = 0; i < 28; i++) {
+        list->push_back(astley[i]);
+        test(list->size() - 1 == (size_t)i);
+        test(list->back() == astley[i]);
+        test(list->front() == astley[0]);
+    }
+    for (int i = 0; i < 28; i++) {
+        test(list->get(i) == astley[i]);
+    }
+    for (int i = 27; i >= 0; i--) {
+        test(list->size() - 1 == (size_t)i);
+        test(list->back() == astley[i]);
+        test(list->front() == astley[0]);
+        list->pop_back();
+    }
+    test(list->size() == 0);
+    test(list->empty());
+    for (int i = 0; i < 28; i++) {
+        list->push_front(astley[27 - i]);
+        test(list->size() - 1 == (size_t)i);
+        test(list->front() == astley[27 - i]);
+        test(list->back() == astley[27]);
+    }
+    for (int i = 0; i < 28; i++) {
+        test(list->get(i) == astley[i]);
+    }
+    for (int i = 27; i >= 0; i--) {
+        test(list->size() - 1 == (size_t)i);
+        test(list->front() == astley[27 - i]);
+        test(list->back() == astley[27]);
+        list->pop_front();
+    }
+    test(list->size() == 0);
+    test(list->empty());
+    delete list;
+    std::cout << "Tests successful\n";
+    return 0;
 }
